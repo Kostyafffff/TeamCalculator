6cdc3a422ecff1161b17627244f64a7e000ccfd6
stefannafets911 <stefannafets71@gmail.com>
2019-08-18 13:11:39 +0300

    make all task's in this case, also need to fix some code style

--- calc/controller.js
+++ calc/controller.js
@@ -1,30 +1,33 @@
 const clickOnNumber = e => {
     if (!e || !e.target.innerHTML) {
-        return
+        return;
     }
+
+    if (model.result !== null) {
+        setToModel(model.result, 'firstValue');
+        setToModel(null, 'result');
+        document.getElementById("inputField").value = "";
+    }
+
     let inputString = document.getElementById('inputField').value;
 
     //проверка на наличие точки в строке
-    if (e.target.innerHTML==='.'){
-        if(inputString.indexOf('.', 0) >= 0){
-            return
-        }
+    if (e.target.innerHTML === '.' && inputString.indexOf('.', 0) >= 0) {
+        return;
     }
 
     //проверка на 0 вначале строки, если вначале стоит 0 и при этом длинна строки не более 1, то выходим из функции
-    if (e.target.innerHTML==='0'){
-        if(inputString.charAt(0) === '0' && inputString.length <= 1){
-            return
-        }
+    if (e.target.innerHTML === '0' && inputString.charAt(0) === '0' && inputString.length <= 1) {
+        return;
     }
 
     //не больше 8 чисел в поле ввода
-    if (inputString.length >= 8){
+    if (inputString.length >= 8) {
         return;
     }
 
     //начальное число всегда 0, но при вводе чего-то другого меняется
-    if (inputString.charAt(0) === '0' && e.target.innerHTML !== '0' && e.target.innerHTML !== '.' && inputString.length <= 1){
+    if (inputString.charAt(0) === '0' && e.target.innerHTML !== '0' && e.target.innerHTML !== '.' && inputString.length <= 1) {
         document.getElementById("inputField").value = "";
     }
 
@@ -33,36 +36,26 @@ const clickOnNumber = e => {
 
 const Clear = e => {
     //если передается true то чищу всю модель кроме результата
-    if (e && e.target.clearAll === true) {
-        setToModel(null, 'firstValue');
-        setToModel(null, 'secondValue');
-        setToModel(null, 'result');
-        setToModel('', 'sign');
-    }
+    setToModel(null, 'firstValue');
+    setToModel(null, 'secondValue');
+    setToModel(null, 'result');
+    setToModel('', 'sign');
+
     document.getElementById("inputField").value = "0";
 };
 
 const clickOnSign = e => {
     if (!e || !e.target.innerHTML) {
-        return
+        return;
     }
 
-    if (model.result === null) {
-        if (model.sign === '') {
-            setToModel(document.getElementById("inputField").value, 'firstValue');
-        } else {
-            setToModel(document.getElementById("inputField").value, 'secondValue');
-        }
-    }else {
-        //прошлый результат переношу в поле первого числа и очищаю поле результата что бы начался новый цикл расчета
-        setToModel(model.result, 'firstValue');
-        setToModel(null, 'result');
-        Clear();
-        // return clickOnNumber();
-
+    if (model.sign === '') {
+        setToModel(document.getElementById("inputField").value, 'firstValue');
+    } else {
+        setToModel(document.getElementById("inputField").value, 'secondValue');
     }
 
-    Clear();
+    document.getElementById("inputField").value = "";
 
     //если знак = то просто считает, если знак иной, то считает учитывая новый знак
     if (e.target.innerHTML !== '=') {
@@ -75,8 +68,9 @@ const clickOnSign = e => {
 
 const makeResult = () => {
     if (model.sign === '' || model.firstValue === null || model.secondValue === null) {
-        return
+        return;
     }
+
     let result = null;
 
     switch (model.sign) {
@@ -94,7 +88,7 @@ const makeResult = () => {
             break;
     }
 
-    result.toPrecision(3);
+    // result.toPrecision(3);
     setToModel(result, 'result');
     document.getElementById("inputField").value = result;
 
@@ -109,22 +103,36 @@ const setToModel = (value, field) => {
     model[field] = value;
 };
 
+const onKeyDown = e => {
+    const grandedKeys = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
+    // const grandedKeys = '0123456789.';
+
+    if (!grandedKeys.includes(e.key)){
+        console.log(e.key);
+        return;
+    }else{
+        return false;
+    }
+
+};
 
 const init = () => {
     numberButtons = document.getElementsByClassName('number-button');
-    for (let i = 0; i < numberButtons.length; i++) {
-        numberButtons[i].addEventListener('click', clickOnNumber, true);
-    }
-
     signButtons = document.getElementsByClassName('sign-button');
-    for (let i = 0; i < signButtons.length; i++) {
-        signButtons[i].addEventListener('click', clickOnSign, true);
+    clearInput = document.getElementById("clearButton");
+    input = document.getElementById('inputField');
+
+    for (let i = 0; i < numberButtons.length; i++) {
+        numberButtons[i].addEventListener('click', clickOnNumber, false);
     }
 
-    clearInput = document.getElementById("clearButton");
-    clearInput.addEventListener("click", Clear, true);
-    clearInput.clearAll = true;
+    for (let i = 0; i < signButtons.length; i++) {
+        signButtons[i].addEventListener('click', clickOnSign, false);
+    }
 
+    clearInput.addEventListener("click", Clear, false);
+
+    // input.addEventListener('keydown', onKeyDown, false);
 };
 
 init();
